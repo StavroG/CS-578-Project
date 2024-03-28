117c6ddf44a9afc6afea4b30d33d68024ffe2c5e
Isaia Pacheco <pachecoisaia@gmail.com>
2024-03-27 21:49:30 -0700

    readmes
    updating read mes

--- Hardware/Arduino/Receiver/Arduino/Arduino.ino
+++ Hardware/Arduino/Receiver/Arduino/Arduino.ino
@@ -1,16 +1,27 @@
 #include "C:\Users\Ipacheco\source\repos\CS-578-Project\Hardware\Arduino\UxcellPhotodiode.cpp"
+#include "C:\Users\Ipacheco\source\repos\CS-578-Project\Hardware\Arduino\HiLetgoRGBColorSensor.cpp"
 
+//UxcellPhotodiode
 #define DIODE_DIGITAL_PIN 4
 #define DIODE_ANALOG_PIN A0
 #define MODE 0 //0 - digital : 1- analog
 
+//HiLetgoRGBColorSensor
+//https://github.com/adafruit/Adafruit_TCS34725/tree/master
+#define INTEGRATION_TIME TCS34725_INTEGRATIONTIME_50MS
+#define GAIN TCS34725_GAIN_4X
+
 using namespace std;
 
-UxcellPhotodiode diode = UxcellPhotodiode(DIODE_DIGITAL_PIN, MODE);
+//UxcellPhotodiode diode = UxcellPhotodiode(DIODE_DIGITAL_PIN, MODE);
+HiLetgoRGBColorSensor colorDiode = HiLetgoRGBColorSensor(INTEGRATION_TIME, GAIN);
 
 void setup() {
   Serial.begin(9600);
-  diode.initialize();
+  //diode.initialize();
+  if (!colorDiode.initialize()) {
+    Serial.println("No TCS34725 found ... check your connections");
+  }
 }
 
 void loop() {
--- Hardware/Arduino/Receiver/README.md
+++ Hardware/Arduino/Receiver/README.md
@@ -1,3 +1,4 @@
+//UxcellPhotodiode
 1. Using red wire connect 5v on board to vcc on uxcell photodiode
 2. Using black wire connect gnd on board to gnd on uxcell photodiode
 3. Using color 3 wire connect to a digital gio pin (i used pin D4 on arduino uno)on board to DO on uxcell photodiode
@@ -5,3 +6,11 @@
 4a. Connect usb 3.0 to pc to flash code to the board
 4b. or connect 5v power source into board if code is flashed onto the board already
 5. Download/Open arduino ide
+
+//HiLetgoRGBSensor
+1. Connect VDD on HiLetgoRGBSensor to 3.3V DC on arduino using red wire
+2. Connect GROUND on HiLetgoRGBSensor to common ground on arduino using black wire
+3. Connect SDA on HiLetgoRGBSensor to analog 4 on arduino using color 3 wire
+4. Connect SCL on HiLetgoRGBSensor to analog 5 on arduino using color 4 wire
+5. Download/Open arduino ide
+6. Download Adafruit TCS34725 by Adafruit libary in Arduido IDE
